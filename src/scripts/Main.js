395ef78a9518e4c51a56fed70fb8b1ffb841ce03
Marc-A. R. <marcantoine.roy03@gmail.com>
2025-11-17 13:01:32 -0500

    cards labos 1-4 fini

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,3 +1,4 @@
+import { Pagination } from 'swiper/modules';
 import Icons from './utils/Icons.js';
 
 class Main {
@@ -7,6 +8,19 @@ class Main {
 
   init() {
     Icons.load();
+
+    this.initSwiperCards();
+  }
+
+  initSwiperCards() {
+    const target = document.querySelector('.js-swiper-cards');
+    const swiper = new Swiper(target, {
+      slidesPerView: 1,
+
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
   }
 }
 new Main();
