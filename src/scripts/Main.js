e3d40045fe6e21a40b5adb2f23d04157c13259e9
Marc-A. R. <marcantoine.roy03@gmail.com>
2025-11-17 13:44:34 -0500

    presque fini le swiper

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,5 +1,5 @@
-import { Pagination } from 'swiper/modules';
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
@@ -15,10 +15,24 @@ class Main {
   initSwiperCards() {
     const target = document.querySelector('.js-swiper-cards');
     const swiper = new Swiper(target, {
-      slidesPerView: 1,
+      slidesPerView: 2,
 
       pagination: {
         el: '.swiper-pagination',
+      },
+
+      breakpoints: {
+        480: {
+          slidesPerView: 1,
+        },
+
+        768: {
+          slidesPerView: 2,
+        },
+
+        1024: {
+          slidesPerView: 1,
+        },
       },
     });
   }
